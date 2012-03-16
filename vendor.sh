2f7c940f0fd2d814b73ec57b6852b9fe015693c6
Elton Minetto <eminetto@gmail.com>
2012-03-16 17:12:13 -0300

    Remove previous version of vendor directory before cloning projects

--- vendor.sh
+++ vendor.sh
@@ -1,12 +1,13 @@
+rm -rf vendor
 mkdir vendor
 cd vendor
 wget http://silex.sensiolabs.org/get/silex.phar
 mkdir -p DMS/Filter
-git clone http://github.com/rdohms/DMS-Filter.git DMS/Filter
+git clone https://github.com/rdohms/DMS-Filter.git DMS/Filter
 mkdir doctrine
-git clone http://github.com/doctrine/common.git doctrine/common
-git clone http://github.com/doctrine/dbal.git doctrine/dbal
-git clone http://github.com/doctrine/doctrine2.git doctrine/orm
+git clone https://github.com/doctrine/common.git doctrine/common
+git clone https://github.com/doctrine/dbal.git doctrine/dbal
+git clone https://github.com/doctrine/doctrine2.git doctrine/orm
 mkdir -p Symfony/Component
 git clone https://github.com/symfony/ClassLoader.git Symfony/Component/Classloader
 git clone https://github.com/symfony/Validator.git Symfony/Component/Validator
